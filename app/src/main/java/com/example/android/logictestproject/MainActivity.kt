ad9e72817f66e9c90595209e17d82a339d02e9dc
Dmitry <mr.dimenter@gmail.com>
2020-04-18 17:23:10 +0400

    Create fragment title and create UI interface for fragment title

--- app/src/main/java/com/example/android/logictestproject/MainActivity.kt
+++ app/src/main/java/com/example/android/logictestproject/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
