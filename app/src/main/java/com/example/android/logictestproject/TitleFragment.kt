6b4491e050ddac6fc06727bb4231e1a4942908a7
Dmitry <mr.dimenter@gmail.com>
2020-04-23 01:02:14 +0400

    made UI for the page about

--- app/src/main/java/com/example/android/logictestproject/TitleFragment.kt
+++ app/src/main/java/com/example/android/logictestproject/TitleFragment.kt
@@ -1,12 +1,11 @@
 package com.example.android.logictestproject
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.logictestproject.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -18,9 +17,24 @@ class TitleFragment : Fragment() {
         binding.startButton.setOnClickListener { v: View ->  v.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)} //listener для перехода на другой фрагмент
 
         binding.InstructionButton.setOnClickListener { v: View -> v.findNavController().navigate(R.id.action_titleFragment_to_instructionFragment)  }
+
+        setHasOptionsMenu(true) //добавляем меню в title bar
+
         return binding.root
         
         
 
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater?.inflate(R.menu.overflow_menu, menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+            view!!.findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
